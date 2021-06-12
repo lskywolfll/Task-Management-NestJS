795331716494663c5cfac907ee78b42dce2e237f
Rene <renealejandrosanchezmorales@gmail.com>
2021-06-12 17:21:29 -0400

    finish feature

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,7 +1,9 @@
 import { Patch } from '@nestjs/common';
 import { Delete, Param } from '@nestjs/common';
+import { Query } from '@nestjs/common';
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { CreateTaskDto } from './dto/create-task.dto';
+import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 import { Task, TaskStatus } from './task.model';
 import { TasksService } from './tasks.service';
 
@@ -10,8 +12,15 @@ export class TasksController {
   constructor(private tasksService: TasksService) {}
 
   @Get()
-  getAllTasks(): Task[] {
-    return this.tasksService.getAllTasks();
+  getTasks(@Query() filterDto: GetTasksFilterDto): Task[] {
+    // if we have any filters defined, call taskSerice.getTasksWilFilters
+    // otherwise, just get all tasks
+
+    if (Object.keys(filterDto).length) {
+      return this.tasksService.getStasksWithFilters(filterDto);
+    } else {
+      return this.tasksService.getAllTasks();
+    }
   }
 
   @Get('/:id')

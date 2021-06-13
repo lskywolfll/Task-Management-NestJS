d4bfcd0e2550d22a06d6eda32e2af9748c95a25d
Rene <renealejandrosanchezmorales@gmail.com>
2021-06-13 19:16:58 -0400

    added packages typeorm and pg to persistance data

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,20 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { TasksModule } from './tasks/tasks.module';
 
 @Module({
-  imports: [TasksModule],
+  imports: [
+    TasksModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: '172.18.0.2',
+      port: 5432,
+      username: 'postgres',
+      password: 'admin',
+      database: 'task-management',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
 })
 export class AppModule {}

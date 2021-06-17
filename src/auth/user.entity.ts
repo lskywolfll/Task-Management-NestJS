2261476c7fb5990e02578c7e06d209536ba97086
Rene <renealejandrosanchezmorales@gmail.com>
2021-06-16 22:11:35 -0400

    added module auth and created repository and entity for this with all configurations

---
+++ src/auth/user.entity.ts
@@ -0,0 +1,14 @@
+import { IsString } from 'class-validator';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  username: string;
+
+  @Column()
+  password: string;
+}
